17a3dba60195cea6fd99d1c14b5e8786bb23c5b8
chaochaowu <chaowu.dev@gmail.com>
2019-08-18 13:23:03 +0800

    Set information validation 1. set validation of task columns 2. extend task_spec 3. add error-messages on page

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,18 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  # 之後改用 FactoryBot
-  let(:task) { Task.new(user_id: 1, title: 'task test', description: 'this is task test', status: 0, started_at: '2019-07-11', deadline_at: '2019-07-12', emergency_level: 0) }
+  let(:task) { create :task }
+  before { task }
 
   describe 'the informations of task must be presence' do
-    it 'task has user_id, title, description, status, started_at, deadline_at, emergency_level' do
+    it 'user must exist' do
+      task.user_id = nil
+      task.valid?
+      expect(task.errors[:user]).to include 'must exist'
+    end
+
+    it 'task has user_id, title, status, started_at, deadline_at, emergency_level' do
       should validate_presence_of :user_id
       should validate_presence_of :title
-      should validate_presence_of :description
       should validate_presence_of :status
       should validate_presence_of :started_at
       should validate_presence_of :deadline_at
       should validate_presence_of :emergency_level
+    end
+
+    it 'title cant\'t not be blank' do
+      task.title = nil
+      task.valid?
+      expect(task.errors[:title]).to include 'can\' t be blank!'
+    end
+  end
+
+  describe 'default value must exist' do
+    it 'status has default value' do
+      expect(task.status).to eq 'to_do'
+    end
+
+    it 'emergency_level has default value' do
+      expect(task.emergency_level).to eq 'unimportant'
+    end
+  end
+
+  describe 'enum can work' do
+    it 'enum of status can work' do
+      task.status = 2
+      expect(task.status).to eq 'done'
+    end
+
+    it 'enum of emergency_level can work' do
+      task.emergency_level = 2
+      expect(task.emergency_level).to eq 'urgent'
     end
   end
   
